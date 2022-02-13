825db27197b7f74130ebdbbaf694d866b184745f
Samariddin <ssamar.96.96@gmail.com>
2022-02-13 08:27:55 +0300

    First commit

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log('Hello world')
+let x = 10
+let y = '10'
+
+console.log(x != y)
+
+console.log(x !== y)
+
+console.log(typeof('o'))
+console.log(typeof(`tcr`))
+console.log(typeof("idj"))
